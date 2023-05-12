c9af6a65436a1b51d70a792bd49eec2b318c7ea7
valpas312 <96994527+valpas312@users.noreply.github.com>
2023-05-12 16:47:29 -0300

    funcionalidad al register para guardar datos en json server

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,80 +1,120 @@
 import { useContext } from "react";
 import { UserContext } from "../App";
-import { Box, Button, FormControl, FormLabel, Heading, Input, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Spinner,
+  Text,
+} from "@chakra-ui/react";
 import { Link, useNavigate } from "react-router-dom";
 import { amarillo, rosaClaro2 } from "../styles/utils/colores";
+import { useMutation } from "@tanstack/react-query";
 
 const Register = () => {
-   // eslint-disable-next-line no-unused-vars
-   const [user, setUser] = useContext(UserContext)
+  // eslint-disable-next-line no-unused-vars
+  const [user, setUser] = useContext(UserContext);
 
-   const navigate = useNavigate()
+  const navigate = useNavigate();
+  
+  const { mutate, isLoading } = useMutation({
+      mutationKey: ["agrearUsuario"],
+      mutationFn: (usuario) => 
+         fetch("http://localhost:4000/usuarios", {
+            method: "POST",
+            body: JSON.stringify(usuario),
+            headers: {
+            "Content-Type": "application/json",
+            },
+        }),
+    });
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+        const nombre = e.target.nombre.value;
+        const apellido = e.target.apellido.value;
+        const correo = e.target.correo.value;
+        const contraseña = e.target.contraseña.value;
+        const tipo = "alumno";
 
-   const handleSubmit = (e) => {
-       e.preventDefault();
-       const nombre = e.target.nombre.value;
-       const apellido = e.target.apellido.value;
-       const correo = e.target.correo.value;
-       const contraseña = e.target.contraseña.value;
-       setUser({ nombre, apellido, correo, contraseña });
-       navigate("/");
-   };
+    const usuario = {
+        nombre,
+        apellido,
+        correo,
+        contraseña,
+        tipo,
+    };
 
- return (
-   <Box
-       display="flex"
-       flexDirection="column"
-       justifyContent="center"
-       alignItems="center"
-       w="100%"
-       gap="1rem"
-   >
-       <Heading>Registrate</Heading>
-       <FormControl
-           as="form"
-           onSubmit={handleSubmit}
-           display="flex"
-           flexDirection="column"
-           justifyContent="center"
-           alignItems="center"
-           gap="1rem"
-           minW="40%"
-           maxW="50%"
-           p={4}
-           borderRadius={8}
-           bg={rosaClaro2}
-           boxShadow={`0px 0px 10px 0px ${amarillo}`}
-       >
-           <FormLabel
-                htmlFor="nombre"
-                fontWeight="bold"
-           >Nombre</FormLabel>
-           <Input type="text" placeholder="Nombre" id="nombre" required />
 
-           <FormLabel
-                htmlFor="apellido"
-                fontWeight="bold"
-           >Apellido</FormLabel>
-           <Input type="text" placeholder="Apellido" id="apellido" required />
+    setUser(usuario);
+    mutate(usuario);
+    navigate("/");
 
-           <FormLabel
-                htmlFor="correo"
-                fontWeight="bold"
-           >Correo</FormLabel>
-           <Input type="email" placeholder="Correo" id="correo" required />
+  };
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      w="100%"
+      gap="1rem"
+    >
+      <Heading>Registrate</Heading>
+      <FormControl
+        as="form"
+        onSubmit={handleSubmit}
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        gap="1rem"
+        minW="40%"
+        maxW="50%"
+        p={4}
+        borderRadius={8}
+        bg={rosaClaro2}
+        boxShadow={`0px 0px 10px 0px ${amarillo}`}
+      >
+        <FormLabel htmlFor="nombre" fontWeight="bold">
+          Nombre
+        </FormLabel>
+        <Input type="text" placeholder="Nombre" id="nombre" required />
 
-           <FormLabel
-                htmlFor="contraseña"
-                fontWeight="bold"
-           >Contraseña</FormLabel>
-           <Input type="password" placeholder="Contraseña" id="contraseña" required/>
+        <FormLabel htmlFor="apellido" fontWeight="bold">
+          Apellido
+        </FormLabel>
+        <Input type="text" placeholder="Apellido" id="apellido" required />
 
-           <Button type="submit" bg={amarillo} _hover={{shadow:"xl"}}>Iniciar sesión</Button>
-       </FormControl>
-       <Text>Ya tenes cuenta? </Text>
-        <Button as={Link} to={"/login"}>Inicia sesión</Button>
-   </Box>
- )
-}
+        <FormLabel htmlFor="correo" fontWeight="bold">
+          Correo
+        </FormLabel>
+        <Input type="email" placeholder="Correo" id="correo" required />
 
-export default Register
+        <FormLabel htmlFor="contraseña" fontWeight="bold">
+          Contraseña
+        </FormLabel>
+        <Input
+          type="password"
+          placeholder="Contraseña"
+          id="contraseña"
+          required
+        />
+
+        <Button type="submit" bg={amarillo} _hover={{ shadow: "xl" }}>
+            {isLoading ? <Spinner /> : "Registrarse"}
+        </Button>
+      </FormControl>
+      <Text>Ya tenes cuenta? </Text>
+      <Button as={Link} to={"/login"}>
+        Inicia sesión
+      </Button>
+    </Box>
+  );
+};
+
+export default Register;
