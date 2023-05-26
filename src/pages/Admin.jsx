5e2478e22ab274dbff13315da6de1c020ec071d9
valpas312 <96994527+valpas312@users.noreply.github.com>
2023-05-26 01:12:09 -0300

    renderizando todas las tareas para los admins

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,7 +1,32 @@
+import { useQuery } from "@tanstack/react-query"
+import { API_URL } from "../utils/API_URL"
+import { Box, Spinner, Text } from "@chakra-ui/react";
+import CardTarea from "../components/CardTarea";
+
 
 const Admin = () => {
+
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ['tareas'],
+    queryFn: () => fetch(`${API_URL}/tareas/`).then((res) => res.json()),
+    refetchOnMount: true,
+    refetchOnWindowFocus: true,
+    refetchOnReconnect: true,
+    refetchInterval: 5000,
+  });
+
   return (
-    <div>Admin</div>
+    <Box display="flex" gap="1rem" flexDirection="column" alignItems="center">
+    <Box w="100%" display="flex" alignItems="center" justifyContent="center" gap="1rem" p="1rem" flexWrap={"wrap"}>
+        {
+            isLoading ? <Spinner /> : isError ? <Text>{error.messagge}</Text> : (
+                data === undefined || data.length === 0 ? <Text>Todavia no hay tareas pendientes ni terminadas</Text> : (data.map((tarea) => (
+                    <CardTarea key={tarea._id} {...tarea} />
+                )))
+            )
+        }
+    </Box>
+    </Box>
   )
 }
 
